6fa6ce82fb382ff58536dc4918b61dc0de806724
DumbWhiz <adeyeyewilson@gmail.com>
2022-10-14 09:57:29 +0100

    form.php restored, index.html file removed
    Files modified

--- PHP/user_data.php
+++ PHP/user_data.php
@@ -1,10 +1,10 @@
 <?php
 if (isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $genders = $_POST['genders'];
-    $country = $_POST['country'];
+    $name = $_POST['name'] . "<br>";
+    $email = $_POST['email'] . "<br>";
+    $password = $_POST['password'] . "<br>";
+    $genders = $_POST['genders'] . "<br>";
+    $country = $_POST['country'] . "<br>";
 
     $data = [$name, $email, $password, $genders, $country];
     
